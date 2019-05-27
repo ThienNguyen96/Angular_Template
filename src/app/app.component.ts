f7d455613337172afd0623aa95afc6913df5d126
ThienNguyen <doithua9669@gmail.com>
2019-05-27 21:04:35 +0700

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'teamplate-angular';
+  imgLink = 'https://fcbarcelona-static-files.s3.amazonaws.com/fcbarcelona/photo/2018/09/21/bb29cbe2-e2a6-450b-9134-0ded29c89f37/10-MESSI-JOC.jpg';
+  isBorder = true;
+  isBackground = false;
 }
