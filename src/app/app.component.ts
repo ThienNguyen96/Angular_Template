533fba724d2f052b9fa42aaf5909e4275f4eb2cd
ThienNguyen <doithua9669@gmail.com>
2019-06-06 11:03:05 +0700

    completed ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'teamplate-angular';
+  isEnabled = false;
+
+  onToggleStyle(){
+    this.isEnabled = !this.isEnabled;
+  }
+  setClasses(){
+    return{
+      'border' : this.isEnabled ? '1px solid blue' : '',
+      'padding.px' : this.isEnabled ? '10' : '',
+      'color' : this.isEnabled ? 'red' : ''
+    }
+  }
 }
