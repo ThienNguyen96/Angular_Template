5e838a8d502981bcc075f40455b8931420cfecdc
ThienNguyen <doithua9669@gmail.com>
2019-06-09 14:38:26 +0700

    import third party lodah

--- src/app/pipe/sort.pipe.ts
+++ src/app/pipe/sort.pipe.ts
@@ -1,4 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import 'lodash';
+declare var _;
 
 @Pipe({
   name: 'sort'
@@ -7,18 +9,10 @@ export class SortPipe implements PipeTransform {
 
   transform(products: any, sortBy: string, sortValue: number): any {
     if(sortBy === 'name'){
-      products.sort((a,b) => {
-        if(a.name > b.name) return sortValue;
-        else if(a.name < b.name) return -sortValue;
-        else return 0;
-      })
+     products = _.orderBy(products, ['name'], [sortValue == 1 ? 'asc' : 'desc']);
     }
     if(sortBy === 'price'){
-      products.sort((a,b) => {
-        if(a.price > b.price) return sortValue;
-        else if(a.price < b.price) return -sortValue;
-        else return 0;
-      })
+      products = _.orderBy(products, ['price'], [sortValue == 1 ? 'asc' : 'desc']);
     }
     return products;
   }
