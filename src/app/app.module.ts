32b00e445483974151c072b84e83e1dbd3ea8006
tma_pvhai <pvhai@tma.com.vn>
2019-08-05 14:36:08 +0700

    clear

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FirstComponent } from './components/first/first.component';
-import { SecondComponent } from './components/second/second.component';
 
-//Service
-import {LoggingService} from './services/logging.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponent,
-    SecondComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [
-    LoggingService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
